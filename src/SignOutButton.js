3caceaf055e4c1c038cab7fa1819b8b66950d5ab
pecchioducharne <pecchioducharne@gmail.com>
2023-12-20 20:04:08 -0500

    Fixing signout

--- src/SignOutButton.js
+++ src/SignOutButton.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "./firebase"; // Import Firebase auth from your firebase.js file
+import { useNavigate } from "react-router-dom";
 import "./SignOutButton.css"; // Create a CSS file for styling
 
 const SignOutButton = () => {
+  const navigate = useNavigate();
+
   const handleSignOut = async () => {
     try {
       await signOut(auth); // Sign the user out
+      navigate("/login");
       // You can add any additional logic after signing out, such as redirecting to the login page
     } catch (error) {
       console.error("Error signing out:", error);
