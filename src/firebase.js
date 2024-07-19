08fdd803f8116d248765fc224182006a5b8649b1
pecchioducharne <pecchioducharne@gmail.com>
2024-07-19 14:36:03 -0400

    Adding back persistence

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,37 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // Import Firestore
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore"; // Import Firestore
 import { getStorage } from "firebase/storage"; // Import Firebase Storage
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID",
+  apiKey: "AIzaSyCmQRXoSyXy5rGm8JjF5JGH_eFQibKW_0g",
+  authDomain: "drafted-6c302.firebaseapp.com",
+  projectId: "drafted-6c302",
+  storageBucket: "drafted-6c302.appspot.com",
+  messagingSenderId: "739427449972",
+  appId: "1:739427449972:web:c02c6a8cdf544c30e52521",
+  measurementId: "G-2C3DWJC6W6",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); // Initialize Firebase Authentication
+setPersistence(auth, browserLocalPersistence); // Enable auth persistence
 const db = getFirestore(app); // Initialize Firestore
 const storage = getStorage(app); // Initialize Firebase Storage
+
+// Enable Firestore persistence
+try {
+  enableIndexedDbPersistence(db);
+} catch (err) {
+  if (err.code === 'failed-precondition') {
+    // Multiple tabs open, persistence can only be enabled
+    // in one tab at a a time.
+    console.log('Multiple tabs open, persistence can only be enabled in one tab at a time.');
+  } else if (err.code === 'unimplemented') {
+    // The current browser does not support all of the
+    // features required to enable persistence
+    console.log('The current browser does not support all of the features required to enable persistence.');
+  }
+}
 
 export { app, auth, db, storage }; // Export app, auth, and db
